6d51634f0330f8d266c08b0efa416d95c18682f1
BirendraRai1 <birendrabit123@gmail.com>
2024-02-13 16:52:21 +0530

    added questions for optional chaining

---
+++ Advanced/javascript/optionalChaining.js
@@ -0,0 +1,4 @@
+Q1>what is optional Chaining?
+Ans>Optional chaining lives up to its name. In the chain of object property access we can check that each value is not undefined or null. This check can be extremely useful when accessing deeply nested object values. It has been a highly anticipated feature and it keeps you from having to do numerous null checks
+
+//https://www.freecodecamp.org/news/javascript-optional-chaining/
