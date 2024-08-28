45dabc3b0357dff5e7b271d59fac11635e0f81ee
BirendraRai1 <birendrabit123@gmail.com>
2024-08-28 17:48:11 +0530

    added questions on sorting

--- Advanced/interviews/chakdeFrontendInterview/17EpisodeLLD.js
+++ Advanced/interviews/chakdeFrontendInterview/17EpisodeLLD.js
@@ -1,1 +1,23 @@
 //www.youtube.com/watch?v=QWTpAsCDoy8&list=PL4CFloQ4GGWIXCJJHwT6FPb5yVZcybuQW&index=15
+
+import React, { ReactElement, useState } from "react";
+
+const Toast = ({ text, onClose }) => {
+  return (
+    <div>
+      <p>{text}</p>
+      <button onClick={onClose}>X</button>
+    </div>
+  );
+};
+const solution = () => {
+  const [toastVisible, setToastVisible] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setToastVisible(true)}>Show Toast</button>
+      <Toast text={"I am Toast"} onClose={() => setToastVisible(false)} />
+    </div>
+  );
+};
+
+export default solution;
